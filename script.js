967b1282f69d386d81d9f5c4ac4d8749e8983f07
k rahul pandit <147794873+rahulpandit-123@users.noreply.github.com>
2026-05-10 16:46:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let boxes = document.getElementsByClassName("box")
+
+        for (let box of boxes) {
+            box.addEventListener("mouseover", (e) => {
+                for(let item of boxes){
+                    if(item != box){
+                        item.classList.add("active")
+                    }
+                }
+            })
+            box.addEventListener("mouseout", ()=>{
+                for(let item of boxes){
+                    if(item != box ){
+                        item.classList.remove("active")
+                       
+                    }
+                }
+            })
+        }
